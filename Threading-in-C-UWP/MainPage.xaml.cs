25b0c7a8d80f6eb0e5e59deb81829da34ba53ca6
Simchaja <53301183+Simchaja@users.noreply.github.com>
2023-04-24 22:34:29 +0200

    second screen added
    added the playerboard

--- Threading-in-C-UWP/MainPage.xaml.cs
+++ Threading-in-C-UWP/MainPage.xaml.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Threading_in_C_UWP.Board;
 using Threading_in_C_UWP.Forms;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.WindowManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -23,14 +30,86 @@ namespace Threading_in_C_UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        //private void onFormClosed(object sender, EventArgs e)
+        //{
+        //    if (Application.== 0)
+        //    {
+        //    }
+        //}
         public MainPage()
         {
             this.InitializeComponent();
+            ApplicationView.PreferredLaunchViewSize = new Size(1280, 800);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            StartPlayerboard();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
         {
-            this.Frame.Navigate(typeof(LootScreenForm));
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
+
+        // List of ValueTuple holding the Navigation Tag and the relative Navigation Page
+        private readonly List<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
+        {
+            ("Home", typeof(Home)),
+            ("Players", typeof(LootScreenForm)),
+            ("Map", typeof(PlayerScreenForm))
+        };
+
+        private void NavView_ItemInvoked(NavigationView sender,
+                                 NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked == true)
+            {
+                NavView_Navigate("settings", args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.InvokedItemContainer != null)
+            {
+                var navItemTag = args.InvokedItemContainer.Tag.ToString();
+                NavView_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        private void NavView_Navigate(
+            string navItemTag,
+            Windows.UI.Xaml.Media.Animation.NavigationTransitionInfo transitionInfo)
+        {
+            Type _page = null;
+            if (navItemTag == "settings")
+            {
+                //_page = typeof(SettingsPage);
+            }
+            else
+            {
+                var item = _pages.FirstOrDefault(p => p.Tag.Equals(navItemTag));
+                _page = item.Page;
+            }
+            // Get the page type before navigation so you can prevent duplicate
+            // entries in the backstack.
+            var preNavPageType = ContentFrame.CurrentSourcePageType;
+
+            // Only navigate if the selected page isn't currently loaded.
+            if (!(_page is null) && !Type.Equals(preNavPageType, _page))
+            {
+                ContentFrame.Navigate(_page, null, transitionInfo);
+            }
+        }
+
+        private async void StartPlayerboard()
+        {
+            AppWindow appWindow = await AppWindow.TryCreateAsync();
+            Frame appWindowContentFrame = new Frame();
+            appWindowContentFrame.Navigate(typeof(PlayerBoard));
+            ElementCompositionPreview.SetAppWindowContent(appWindow, appWindowContentFrame);
+            appWindow.Presenter.RequestPresentation(AppWindowPresentationKind.FullScreen);
+            await appWindow.TryShowAsync();
+        }
+
+        //private IAsyncAction ExecuteOnUIThread(DispatchedHandler action)
+        //{
+        //    //return Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, action);
+        //    return Windows.ApplicationModel.Core.CoreApplication.Views
+        //}
     }
 }
