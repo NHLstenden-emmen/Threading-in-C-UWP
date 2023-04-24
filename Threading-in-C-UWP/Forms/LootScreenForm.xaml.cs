b7fefa4276f28a59f8aad5607f6ec1c21eb0c929
Simchaja <53301183+Simchaja@users.noreply.github.com>
2023-04-24 18:23:29 +0200

    start navigationbar

--- Threading-in-C-UWP/Forms/LootScreenForm.xaml.cs
+++ Threading-in-C-UWP/Forms/LootScreenForm.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Threading_in_C_UWP.Forms;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -14,23 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Threading_in_C_UWP
+namespace Threading_in_C_UWP.Forms
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class LootScreenForm : Page
     {
-        public MainPage()
+        public LootScreenForm()
         {
             this.InitializeComponent();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(LootScreenForm));
         }
     }
 }
